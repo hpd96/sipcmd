c8cdbc47d5099443d414f139d885031afeeb5087
a.kluev <a.kluev@php73.ru>
2018-08-21 17:46:23 +0400

    build script

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker rmi sipcmd
+docker build -t sipcmd .
--- sipcmd.sh
+++ sipcmd.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --cap-add=sys_nice --rm sipcmd -P sip  -t 60 -w sip.zadarma.com -u $1 -c $2 -x $3
+docker run --cap-add=sys_nice --rm sipcmd -P sip -t 60 -w sip.zadarma.com -u $1 -c $2 -x $3
